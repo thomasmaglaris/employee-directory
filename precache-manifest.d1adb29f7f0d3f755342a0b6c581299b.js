598fc6a928b30933112673f2dcc9a52d27828aae
tmagz <thomasmaglaris@gmail.com>
2020-11-10 00:50:12 +1030

    Updates

---
+++ precache-manifest.d1adb29f7f0d3f755342a0b6c581299b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c703d97aca5f2495655a28c01b4ec8e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1bb59d2aedf45e302fe",
+    "url": "/static/css/2.e712defa.chunk.css"
+  },
+  {
+    "revision": "148fa4146bff2328da1d",
+    "url": "/static/css/main.69437116.chunk.css"
+  },
+  {
+    "revision": "d1bb59d2aedf45e302fe",
+    "url": "/static/js/2.6aa99a04.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.6aa99a04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "148fa4146bff2328da1d",
+    "url": "/static/js/main.c0a84d5c.chunk.js"
+  },
+  {
+    "revision": "3eaec5ba2d859ccbd2c8",
+    "url": "/static/js/runtime-main.d68be137.js"
+  }
+]);
